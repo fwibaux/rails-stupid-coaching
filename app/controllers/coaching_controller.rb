49b0172b1a742e6667d57ba56707f639f7a3e2fa
Fred Wibaux <fwibaux@gmail.com>
2016-08-08 15:00:08 +0200

    Exercize 1 done Stupid-Coach

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,39 @@
+class CoachingController < ApplicationController
+
+  def home
+  end
+
+  def ask
+
+  end
+
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+
+  private
+
+  def coach_answer(your_message)
+    # TODO: return coach answer to your_message
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    # TODO: return coach answer to your_message, with additional custom rules of yours!
+    if your_message.include? "?"
+      return "I can feel your motivation! Silly question, get dressed and go to work!"
+    elsif your_message == "I am going to work right now!".upcase
+      return ""
+    else
+      return "I can feel your motivation! I don't care, get dressed and go to work!"
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'coaching/ask', to: 'coaching#ask'
+
+  get 'coaching/answer', to: 'coaching#answer'
+
+  root to: 'coaching#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
